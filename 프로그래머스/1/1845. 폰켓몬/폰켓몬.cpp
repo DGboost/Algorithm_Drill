60144b434f486372e1cd545b8e4c1779ae530106
DGboost <147890917+DGboost@users.noreply.github.com>
2024-01-11 03:44:14 +0900

    [level 1] Title: 폰켓몬, Time: 0.18 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    unordered_map<int, int> hash;
+
+    for (auto num: nums)
+        ++hash[num]; 
+
+    return min(hash.size(), nums.size() / 2);
+
+}
