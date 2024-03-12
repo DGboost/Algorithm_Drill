db30a9010d4a4662571dcd15de7302d86db48b2b
DGboost <147890917+DGboost@users.noreply.github.com>
2024-03-12 21:28:12 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
@@ -1,27 +1,25 @@
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> emergency) 
 {
+    vector<int> answer;
     vector<int> temp = emergency;
+    unordered_map<int, int> hash;
     
-    sort(temp.begin(), temp.end());
-    reverse(temp.begin(), temp.end());
+    // emergency copy, sort
+    sort(temp.begin(), temp.end(), greater<>());
+    
+    // hash order set
+    for (int i = 0; i < temp.size(); ++i)
+        hash[temp[i]] = i + 1;
     
     for (auto& emerge : emergency)
-    {
-        for (int i = 0; i < temp.size(); ++i)
-        {
-            if (emerge == temp[i])
-            {
-                emerge = i + 1;
-                break;
-            }
-        }
-    }
-    
-    return emergency;
+        answer.push_back(hash[emerge]);
+        
+    return answer;
 }
