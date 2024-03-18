ccf4c06d32bcb8179a94fe1ed5195f03aab714d6
DGboost <147890917+DGboost@users.noreply.github.com>
2024-03-18 20:42:32 +0900

    [level 1] Title: 모의고사, Time: 3.17 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 36.8 MB, 시간: 3.35 ms
+메모리: 36.8 MB, 시간: 3.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 18일 20:41:05
+2024년 03월 18일 20:42:31
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -2,28 +2,28 @@ function solution(answers) {
     const one = [1, 2, 3, 4, 5];
     const two = [2, 1, 2, 3, 2, 4, 2, 5];
     const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    
+
     let scores = [0, 0, 0]; 
-    
+
     for (let i = 0; i < answers.length; ++i) {
         if (answers[i] === one[i % 5]) 
-            scores[0] += 1;
-        
+            ++scores[0];
+
         if (answers[i] === two[i % 8]) 
-            scores[1] += 1;
-        
+            ++scores[1];
+
         if (answers[i] === three[i % 10]) 
-            scores[2] += 1;
+            ++scores[2];
     }
-    
+
     const maxScore = Math.max(...scores); 
     let result = [];
-    
+
     for (let i = 0; i < scores.length; ++i) {
         if (scores[i] === maxScore) {
             result.push(i + 1);
         }
     }
-    
+
     return result; 
 }
