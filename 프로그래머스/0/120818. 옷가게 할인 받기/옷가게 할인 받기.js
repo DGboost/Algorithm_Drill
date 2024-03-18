b0071fdb143c5767b2e68a8a6cdfe9ee3f2e041c
DGboost <147890917+DGboost@users.noreply.github.com>
2024-03-18 16:02:44 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,13 @@
+function solution(price) {
+    let discount = 0;
+    
+    if (price >= 500000)
+        discount = 0.2;
+    else if (price >= 300000)
+        discount = 0.1;
+    else if (price >= 100000)
+        discount = 0.05;
+    
+    const answer = price - (price * discount);
+    return Math.floor(answer);
+}
