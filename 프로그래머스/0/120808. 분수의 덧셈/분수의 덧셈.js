4ae1c601e81cef0f6e3549c35a75680600af8574
DGboost <147890917+DGboost@users.noreply.github.com>
2024-03-18 14:35:32 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,4 +1,8 @@
-const fnGcd = (num1, num2) => (num1 % num2) ? fnGcd(num2, num1 % num2) : num2;
+const fnGcd = (num1, num2) => {
+    if (num2 === 0)
+        return num1;
+    return fnGcd(num2, num1 % num2);
+}
 
 const solution = (numer1, denom1, numer2, denom2) => {
     let newNumer = (numer1 * denom2) + (numer2 * denom1);
