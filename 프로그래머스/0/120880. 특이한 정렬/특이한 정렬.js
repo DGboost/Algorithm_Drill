32aff3fc074a590f08e9adaa67d44e6c38194511
DGboost <147890917+DGboost@users.noreply.github.com>
2024-03-21 23:56:38 +0900

    [level 0] Title: 특이한 정렬, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,13 +1,3 @@
 function solution(numlist, n) {
-    numlist.sort((a, b) => {
-        const distanceA = Math.abs(a - n);
-        const distanceB = Math.abs(b - n);
-        
-        if (distanceA === distanceB)
-            return b - a;
-        
-        return distanceA - distanceB;
-    });
-
-    return numlist;
+    return numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a);
 }
