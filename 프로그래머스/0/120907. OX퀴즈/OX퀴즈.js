4922d7054eefa5cb716535a687e6eb34df3bf6f6
DGboost <147890917+DGboost@users.noreply.github.com>
2024-03-22 13:10:10 +0900

    [level 0] Title: OX퀴즈, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -0,0 +1,17 @@
+function solution(quiz) {
+    const answer = [];
+    
+    for (let val of quiz) {
+        const [x, sign, y, , z] = val.split(" ");
+        
+        let sum = 0;
+        if (sign === "+") 
+            sum = Number(x) + Number(y);
+        else 
+            sum = Number(x) - Number(y);
+        
+        sum === Number(z) ? answer.push("O") : answer.push("X");
+    }
+    
+    return answer;
+}
